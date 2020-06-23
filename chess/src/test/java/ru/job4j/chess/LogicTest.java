cd048c8b3d5e514ff02a80e855bf52ea63b4d3cf
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-06-23 17:59:12 +0300

    2. Каркас шахматной доски[792#228926]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -9,12 +9,26 @@ import static org.junit.Assert.*;
 
 public class LogicTest {
 
-    @Ignore
+    //@Ignore
     @Test
     public void move() {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
-        boolean rsl = logic.move(Cell.C1, Cell.H6);
+        boolean rsl = logic.move(Cell.C1, Cell.G5);
         assertThat(rsl, is(true));
     }
-}
+
+    @Test
+    public void position() {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C1));
+
+        for (int i = 0; i < )
+            for (Logic x: logic
+            ) {
+
+////        }
+//        //boolean rsl = //logic.move(Cell.C1, Cell.H6);
+////        assertThat(rsl, is(true));
+//    }
+            }
